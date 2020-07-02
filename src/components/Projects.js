a7a30a26b7545b6628d26b01fe02c6503bdaa534
Antonio Lo Fiego <al3359@drexel.edu>
2020-07-02 02:55:44 -0400

    Define projects scaffold

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Project from './Project';
+
+const Projects = ({ data }) => {
+    return (
+        <div className='content-center mx-auto max-w-4xl pb-6'>
+            <div className='mx-10'>
+                <span className='section-header'>Projects</span>
+                {data && data.map((item, i) => <Project item={item} i={i} />)}
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
