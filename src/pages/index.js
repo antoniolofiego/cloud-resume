b284e32471e4c29a229ccbd7e49df4f0c23b5cc2
Antonio Lo Fiego <al3359@drexel.edu>
2020-07-02 03:53:38 -0400

    Implement Grid layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Experience from '../components/Experience';
 import ProjectGroup from '../components/ProjectGroup';
+import Skills from '../components/skills';
 
 import SEO from '../components/SEO';
 import '../styles.css';
@@ -17,8 +18,15 @@ const IndexPage = () => (
             role={resume.role}
             contacts={resume.contacts}
         />
-        <Experience data={resume.experience} />
-        <ProjectGroup data={resume.projects} />
+        <div className='lg:grid lg:grid-col-8 lg:max-w-7xl lg:mx-auto'>
+            <div className='lg:col-start-1 lg:col-end-6'>
+                <Experience data={resume.experience} />
+                <ProjectGroup data={resume.projects} />
+            </div>
+            <div className='lg:col-start-6 lg:col-end-9'>
+                <Skills data={resume.skills} />
+            </div>
+        </div>
     </div>
 );
 
