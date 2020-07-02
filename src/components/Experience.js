f0a0ee1f9c40460c3b475becd3229bec8d292e97
Antonio Lo Fiego <al3359@drexel.edu>
2020-07-02 02:55:23 -0400

    Refactor section header

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,11 +4,9 @@ import Job from './Job';
 
 const Experience = ({ data }) => {
     return (
-        <div className='content-center mx-auto max-w-7xl pb-6'>
+        <div className='content-center mx-auto max-w-4xl pb-6'>
             <div className='mx-10'>
-                <span className='text-2xl text-blue-700 py-2 pr-5 border-b-2 border-blue-200 w-40'>
-                    Experience
-                </span>
+                <span className='section-header'>Experience</span>
                 {data && data.map((item, i) => <Job item={item} i={i} />)}
             </div>
         </div>
