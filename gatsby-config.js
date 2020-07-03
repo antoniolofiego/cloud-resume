c03ca08c8fa4ceef786b7de6e98dbfe547133c55
Antonio Lo Fiego <al3359@drexel.edu>
2020-07-02 21:45:26 -0400

    Fix gatsby-config filesystem issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,6 @@ module.exports = {
         author: `@antonio_lofiego`,
     },
     plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         `gatsby-plugin-postcss`,
